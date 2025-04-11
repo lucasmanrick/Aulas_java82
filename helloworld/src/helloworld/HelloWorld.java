32ef8180c7e2d6e3cd2fef5cbf7b8d33acea8b86
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2025-04-11 11:15:42 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+        //O comando print ln ja pula linha para passar a mensagem  obs: se digitarmos sysout ou syso e ctl + espaço o proprio java ja completa esse codigo de system.out.printls().
+		System.out.println("A turma 82 de Java é top");
+		
+		
+	}
+
+}
